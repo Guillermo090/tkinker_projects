0723cb17fde41e6283be5372c1f409dcc5bbad20
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-01-09 21:23:00 -0300

    add: form functions

--- modern_design/formularios/form_maestro_design.py
+++ modern_design/formularios/form_maestro_design.py
@@ -4,6 +4,7 @@ from config import COLOR_BARRA_SUPERIOR, COLOR_CUERPO_PRINCIPAL, COLOR_MENU_CURS
 import util.util_ventana as util_ventana
 import util.util_imagenes as util_img
 
+
 class FormularioMaestroDesign(tk.Tk):
 
     def __init__(self):
@@ -13,12 +14,14 @@ class FormularioMaestroDesign(tk.Tk):
         self.config_windows()
         self.paneles()
         self.controles_barra_superior()
+        self.controles_menu_lateral()
+        self.controles_cuerpo()
 
     def config_windows(self):
         # configuracion inicial de ventana
         self.title('Python Design')
         self.iconbitmap("./imagenes/logo_pruebas.ico")
-        w, h = 1024, 600
+        w, h = 1024, 860
         util_ventana.centrar_ventana(self, w, h)
 
     def paneles(self):
@@ -41,7 +44,66 @@ class FormularioMaestroDesign(tk.Tk):
         self.labelTitulo.config(fg='#fff', font=("Roboto", 15), bg=COLOR_BARRA_SUPERIOR, pady=10, width=16)
         self.labelTitulo.pack(side=tk.LEFT)
 
-        self.buttonMenuLateral = tk.Button(self.barra_superior, text="\uf0c9", font=font_awesome, bd=0, bg=COLOR_BARRA_SUPERIOR, fg="white")
+        self.buttonMenuLateral = tk.Button(self.barra_superior, text="\uf0c9", font=font_awesome, 
+                                           command=self.toggle_panel, bd=0, bg=COLOR_BARRA_SUPERIOR, fg="white")
         self.buttonMenuLateral.pack(side=tk.LEFT)
 
-        
+        # Etiqueta de informacion
+        self.labelTitulo = tk.Label(self.barra_superior, text='servicio@autodidacta.mx')
+        self.labelTitulo.config(fg="#fff", font=("Roboto", 10),  bg=COLOR_BARRA_SUPERIOR, padx=10, width=20)
+        self.labelTitulo.pack(side=tk.RIGHT)
+
+    def controles_menu_lateral(self):
+        # Configuracion del menu lateral
+        ancho_menu = 20
+        alto_menu = 2
+        font_awesome = font.Font(family='FontAwesome', size=15)
+
+        # etiqueta de perfil
+        self.labelPerfil = tk.Label(self.menu_lateral, image=self.perfil, bg=COLOR_MENU_LATERAL)
+        self.labelPerfil.pack(side=tk.TOP, pady=10)
+
+        # botones del menu lateral
+        self.buttonDashBoard = tk.Button(self.menu_lateral)
+        self.buttonProfile = tk.Button(self.menu_lateral)
+        self.buttonPicture = tk.Button(self.menu_lateral)
+        self.buttonInfo = tk.Button(self.menu_lateral)
+        self.buttonSettings = tk.Button(self.menu_lateral)
+
+        buttons_info = [
+            ("Dashboard", "\uf109", self.buttonDashBoard),
+            ("Profile", "\uf007", self.buttonProfile),
+            ("Picture", "\uf03e", self.buttonPicture),
+            ("Info", "\uf129", self.buttonInfo),
+            ("Settings", "\uf013", self.buttonSettings),
+        ]
+
+        for text, icon, button in buttons_info:
+            self.configurar_boton_menu(button, text, icon, font_awesome, ancho_menu, alto_menu)
+
+    def controles_cuerpo(self):
+        label = tk.Label(self.cuerpo_principal, image=self.logo, bg=COLOR_CUERPO_PRINCIPAL)
+        label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    def configurar_boton_menu(self, button, text, icon, font_awesome, ancho_menu, alto_menu):
+        button.config(text=f"  {icon}  {text}", anchor="w", font=font_awesome, bd=0, bg=COLOR_MENU_LATERAL,
+                      fg="white", width=ancho_menu, height=alto_menu)
+        button.pack(side=tk.TOP)
+        self.bind_hover_events(button)
+    
+    def bind_hover_events(self, button):
+        button.bind("<Enter>", lambda event: self.on_enter(event, button))
+        button.bind("<Leave>", lambda event: self.on_leave(event, button))
+
+    def on_enter(self, event, button):
+        button.config(bg=COLOR_MENU_CURSOR_ENCIMA, fg='white')
+
+    def on_leave(self, event, button):
+        button.config(bg=COLOR_MENU_LATERAL, fg='white')
+
+    def toggle_panel(self):
+        # alternar visibilidad del menu lateral
+        if self.menu_lateral.winfo_ismapped():
+            self.menu_lateral.pack_forget()
+        else:
+            self.menu_lateral.pack(side=tk.LEFT, fill='y')
